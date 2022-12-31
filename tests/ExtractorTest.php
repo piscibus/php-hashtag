6c14e05284ceca806319aceaa913eff4227042d1
Mohamed Gamal <mg.dev1992@gmail.com>
2022-12-31 21:34:26 +0200

    update the test cases

--- tests/ExtractorTest.php
+++ tests/ExtractorTest.php
@@ -13,9 +13,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_english_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract(
+        $actual = Extractor::getHashtags(
             'This is a #test and another #test2 and #foo_bar and #FooBar and #foo@bar #__ #foo__bar #2023 #👋 #foo_👋'
         );
 
@@ -30,9 +28,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_arabic_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract('هذا هو #تجربة و #تجربة2 و #foo_bar و #FooBar و #foo و #كلمة_و_أخرى');
+        $actual = Extractor::getHashtags('هذا هو #تجربة و #تجربة2 و #foo_bar و #FooBar و #foo و #كلمة_و_أخرى');
 
         $this->assertEquals(
             ['#تجربة', '#تجربة2', '#foo_bar', '#FooBar', '#foo', '#كلمة_و_أخرى'],
@@ -45,9 +41,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_french_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract('Ceci est un #test et un autre #test2 et #foo_bar et #FooBar et #foo et #superposés');
+        $actual = Extractor::getHashtags('Ceci est un #test et un autre #test2 et #foo_bar et #FooBar et #foo et #superposés');
 
         $this->assertEquals(
             ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#superposés'],
@@ -60,9 +54,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_german_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract(
+        $actual = Extractor::getHashtags(
             'Dies ist ein #test und ein anderer #test2 und #foo_bar und #FooBar und #foo und #überlappend'
         );
 
@@ -77,9 +69,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_turkish_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract('Bu bir #test ve başka bir #test2 ve #foo_bar ve #FooBar ve #foo ve #üstüste');
+        $actual = Extractor::getHashtags('Bu bir #test ve başka bir #test2 ve #foo_bar ve #FooBar ve #foo ve #üstüste');
 
         $this->assertEquals(
             ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#üstüste'],
@@ -92,9 +82,7 @@ class ExtractorTest extends TestCase
      */
     public function extract_with_spanish_text(): void
     {
-        $sut = new Extractor();
-
-        $actual = $sut->extract('Esto es un #test y otro #test2 y #foo_bar y #FooBar y #foo y #superpuestos');
+        $actual = Extractor::getHashtags('Esto es un #test y otro #test2 y #foo_bar y #FooBar y #foo y #superpuestos');
 
         $this->assertEquals(
             ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#superpuestos'],
