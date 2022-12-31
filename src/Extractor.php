789fa8b6822c5caaab2633cee5ac32424986de01
Mohamed Gamal <mg.dev1992@gmail.com>
2022-12-31 21:23:37 +0200

    init the service

--- src/Extractor.php
+++ src/Extractor.php
@@ -6,4 +6,18 @@ namespace Piscibus\PhpHashtag;
 
 class Extractor
 {
+    /**
+     * Extract the hashtag from a string.
+     */
+    public static function getHashtags(string $text): array
+    {
+        $hashtags = [];
+        preg_match_all('/(#[\p{L}\d_]+)/u', $text, $matches);
+
+        foreach ($matches[0] as $match) {
+            $hashtags[] = $match;
+        }
+
+        return $hashtags;
+    }
 }
