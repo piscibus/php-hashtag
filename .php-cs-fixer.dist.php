84f0e540daf8518431d0faff08bd004369f8a4cc
Mohamad Eldhemy <imdhemy@gmail.com>
2022-12-31 16:48:49 +0200

    initial commit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+$includedPatterns = [
+    __DIR__ . '/src',
+    __DIR__ . '/tests',
+];
+
+$finder = (new PhpCsFixer\Finder())
+    ->in($includedPatterns)
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new PhpCsFixer\Config();
+$config->setRiskyAllowed(true);
+
+return $config
+    ->setRules([
+        '@Symfony' => true,
+        // > PHPUnit
+        'php_unit_method_casing' => ['case' => 'snake_case'],
+        // > Strict
+        'declare_strict_types' => true,
+        // > Operator
+        'not_operator_with_successor_space' => true,
+        // > Cast Notation
+        'cast_spaces' => ['space' => 'none'],
+        // > Import
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+    ])->setFinder($finder);
