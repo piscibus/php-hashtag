6060b4294ae9d2041d73839249235a3dda23c197
Mohamed Gamal <mg.dev1992@gmail.com>
2023-01-03 01:29:18 +0200

    clean up for test cases

--- tests/ExtractorTest.php
+++ tests/ExtractorTest.php
@@ -7,9 +7,9 @@ use function PHPUnit\Framework\assertEquals;
 use Piscibus\PhpHashtag\Extractor;
 
 test('extract with english text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract(
+    $actual = $sut->extract(
         'This is a #test and another #test2 and #foo_bar and #FooBar and #foo@bar #__ #foo__bar #2023 #👋 #foo_👋'
     );
 
@@ -20,9 +20,9 @@ test('extract with english text', function () {
 });
 
 test('extract with arabic text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract('هذا هو #تجربة و #تجربة2 و #foo_bar و #FooBar و #foo و #كلمة_و_أخرى');
+    $actual = $sut->extract('هذا هو #تجربة و #تجربة2 و #foo_bar و #FooBar و #foo و #كلمة_و_أخرى');
 
     assertEquals(
         ['#تجربة', '#تجربة2', '#foo_bar', '#FooBar', '#foo', '#كلمة_و_أخرى'],
@@ -31,9 +31,9 @@ test('extract with arabic text', function () {
 });
 
 test('extract with french text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract('Ceci est un #test et un autre #test2 et #foo_bar et #FooBar et #foo et #superposés');
+    $actual = $sut->extract('Ceci est un #test et un autre #test2 et #foo_bar et #FooBar et #foo et #superposés');
 
     assertEquals(
         ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#superposés'],
@@ -42,9 +42,9 @@ test('extract with french text', function () {
 });
 
 test('extract with german text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract(
+    $actual = $sut->extract(
         'Dies ist ein #test und ein anderer #test2 und #foo_bar und #FooBar und #foo und #überlappend'
     );
 
@@ -55,9 +55,9 @@ test('extract with german text', function () {
 });
 
 test('extract with turkish text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract('Bu bir #test ve başka bir #test2 ve #foo_bar ve #FooBar ve #foo ve #üstüste');
+    $actual = $sut->extract('Bu bir #test ve başka bir #test2 ve #foo_bar ve #FooBar ve #foo ve #üstüste');
 
     assertEquals(
         ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#üstüste'],
@@ -66,9 +66,9 @@ test('extract with turkish text', function () {
 });
 
 test('extract with spanish text', function () {
-    $extractor = new Extractor();
+    $sut = new Extractor();
 
-    $actual = $extractor->extract('Esto es un #test y otro #test2 y #foo_bar y #FooBar y #foo y #superpuestos');
+    $actual = $sut->extract('Esto es un #test y otro #test2 y #foo_bar y #FooBar y #foo y #superpuestos');
 
     assertEquals(
         ['#test', '#test2', '#foo_bar', '#FooBar', '#foo', '#superpuestos'],
