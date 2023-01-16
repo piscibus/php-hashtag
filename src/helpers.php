32542f94b6b7e2a1469b0a4cc9f0cae4a0777250
Mohamad Eldhemy <imdhemy@gmail.com>
2023-01-16 12:07:55 +0200

    fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Piscibus\PhpHashtag\Extractor;
 
 if (! function_exists('extract_hashtags')) {
-    function extract_hashtags(string $text): array {
+    function extract_hashtags(string $text): array
+    {
         return Extractor::extract($text);
     }
 }
